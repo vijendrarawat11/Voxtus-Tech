7a37b3df4328d97af79a3eb0a91bed8afa30ae14
Vijendra Rawat <vijendrarawat11@gmail.com>
2023-06-20 21:20:08 +0530

    Commit

---
+++ Voxtus Tech/Controllers/ContactUs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voxtus_Tech.Controllers
+{
+    public class ContactUs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
