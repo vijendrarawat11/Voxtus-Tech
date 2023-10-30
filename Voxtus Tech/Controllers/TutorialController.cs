8c982c6ca1b49fe472781ac7558e4b4b3079ec16
shivani <shivanigusain2251@gmail.com>
2023-10-30 17:50:26 +0530

    Add learn option and turorial pages

---
+++ Voxtus Tech/Controllers/TutorialController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Voxtus_Tech.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/Introduction-to-power-apps")]
+        public IActionResult Introduction()
+        {
+            return View();	
+        }
+        [Route("learn/canvas-application/setting-up-your-power-apps-environment")]
+        public IActionResult SettingupEnvironment()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/exploring-the-Power-Apps")]
+        public IActionResult ExploringthePowerApps()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/how-to-create-new(environment,-publisher,-solution)")]
+        public IActionResult EnvironmentPublisherSolution()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/understanding-data-source-(dataverse)")]
+        public IActionResult DataSource()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/creating-first-canvas-app)")]
+        public IActionResult CreatingCanvasapp()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/canvas-app-interface")]
+        public IActionResult CanvasAppInterface()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/layout-and-components")]
+        public IActionResult Layoutandcomponents()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/controls-and-properties")]
+        public IActionResult ControlsandProperties()
+        {
+            return View();
+        }
+        [Route("learn/canvas-application/build-a-canvas-application-quickly")]
+        public IActionResult BuildCanvasAppQuickly()
+        {
+            return View();
+        }
+    }
+}
