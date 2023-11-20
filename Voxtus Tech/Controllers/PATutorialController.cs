2a73d1e87e8981a8136e7e154740d6f0b27d7bee
shivani <shivanigusain2251@gmail.com>
2023-11-20 17:38:49 +0530

    Add power Automate Tutorial layout pages and also Add pages ,Add team section in about page, did some changes on navbar

---
+++ Voxtus Tech/Controllers/PATutorialController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voxtus_Tech.Controllers
+{
+    public class PATutorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/introduction-to-power-automate")]
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/licensing-and-pricing-information")]
+        public IActionResult Licensingandpricing()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/key-concepts-and-terminology")]
+        public IActionResult KeyConceptsandTerminology()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/accessing-power-automate")]
+        public IActionResult AccessingPowerAutomate()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/connectors")]
+        public IActionResult Connectors()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/triggers")]
+        public IActionResult Triggers()
+        {
+            return View();
+        }
+        [Route("learn/power-automate/actions")]
+        public IActionResult Actions()
+        {
+            return View();
+        }
+    }
+}
