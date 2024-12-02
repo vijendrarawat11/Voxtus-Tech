2bb294b9ba57d82eeb4bf9501b2fcc65084b2ae2
Shivani Gusain <shivanigusain2251@gmail.com>
2024-12-02 16:56:50 +0530

    Add Blogs

---
+++ Voxtus Tech/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Voxtus_Tech.Models
+{
+    public class LoginModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
